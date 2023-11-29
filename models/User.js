3c737ccc735c831591e2d5b0869526d0361261c0
hsyntes <se.hsyntes@gmail.com>
2023-11-29 08:46:34 +0200

    user schema starter

--- models/User.js
+++ models/User.js
@@ -2,6 +2,18 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   // Schema design & data modeling for users
+  firstname: {
+    type: String,
+    minlength: [2, "Firstname cannot be shorter than 2 characters."],
+    maxlength: [16, "Firstname cannot be longer than 16 characters."],
+    required: [true, "Firstname is required."],
+    trim: true,
+  },
+
+  // implement lastname, username, department, email, password, passwordConfirm
+  // or what else?
+
+  // ...
 });
 
 // * Creating model for users with 'unique' key
