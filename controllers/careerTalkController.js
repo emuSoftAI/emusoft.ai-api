a03422bba65ca7adfb4c240c77d65353c56e5b41
hsyntes <se.hsyntes@gmail.com>
2023-12-26 05:59:06 +0200

    update general layout

---
+++ controllers/careerTalkController.js
@@ -0,0 +1,9 @@
+const CareerTalk = require("../models/CareerTalk");
+
+exports.getCareerTalks = async (req, res, next) => {};
+
+exports.createCareerTalk = async (req, res, next) => {};
+
+exports.getCareerTalk = async (req, res, next) => {};
+
+exports.updateCareerTalk = async (req, res, next) => {};
